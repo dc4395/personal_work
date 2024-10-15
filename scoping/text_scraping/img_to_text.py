129aa8e536cd1506f49c8c189adf8b5b9407cd4c
dc4395 <daoudchaudry95@gmail.com>
2024-10-15 15:58:17 +0100

    testing

---
+++ scoping/text_scraping/img_to_text.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import easyocr
+
+###############################################################
+
+#JPEG Example
+
+###############################################################
+
+jpg_path = r"./scoping/text_scraping/sale_label_example.jpg"
+
+reader = easyocr.Reader(['en'])
+result = reader.readtext(jpg_path, detail = 0)
+print(result)
